c250efbaf902f89daccce8b58d2ba19a70bf362e
Cahangir Kazimzada <78vahjh@code.edu.az>
2023-04-19 23:15:37 +0400

    Dom task added

---
+++ DOM/Dom task/app.js
@@ -0,0 +1,62 @@
+// document.getElementById("item").style.backgroundColor = "yellow";
+// document.querySelectorAll(".item")[0].style.backgroundColor = "red";
+// document.querySelectorAll(".item")[1].style.backgroundColor = "red";
+
+
+// const blueBtn = document.getElementById("blue");
+// const redBtn = document.getElementById("red");
+// const greenBtn = document.getElementById("green");
+// const box = document.querySelector(".box");
+// function eventlisteners() {
+//     blueBtn.style.cssText = `
+//     background-color: blue;
+//     border: none;
+// `;
+//     redBtn.style.cssText = `
+//     background-color: red;
+//     border: none;
+// `;
+//     greenBtn.style.cssText = `
+//     background-color: green;
+//     border: none;
+// `;
+
+//     blueBtn.onclick = () => {
+//         box.style.cssText = `
+//         width: 100px;
+//         height: 100px;
+//         background-color: blue;
+//         margin: 50px;
+//     `
+//     };
+//     redBtn.onclick = () => {
+//         box.style.cssText = `
+//         width: 100px;
+//         height: 100px;
+//         background-color: red;
+//         margin: 50px;
+//     `
+//     };
+//     greenBtn.onclick = () => {
+//         box.style.cssText = `
+//         width: 100px;
+//         height: 100px;
+//         background-color: green;
+//         margin: 50px;
+//     `
+//     };
+// }
+// eventlisteners();
+
+// const btn = document.querySelector(".btn");
+// const box = document.querySelector(".box");
+
+// function randomColors(arr){
+//     return arr[Math.floor(Math.random() * arr.length)];
+// }
+
+// let arr = ["red", "blue", "green", "yellow", "black", "purple", "pink", "aqua"];
+// btn.addEventListener("click", () => {
+//     box.style.backgroundColor = randomColors(arr);
+// })
+
--- DOM/app.js
+++ DOM/app.js
@@ -1,27 +1,26 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2")
-const box = document.querySelector(".box");
+const input = document.getElementById("input");
+const btn = document.getElementById("add");
+const container = document.querySelector(".container");
+const ulElement = document.querySelector(".list");
 
-btn1.addEventListener("click", () => {
-    box.style.cssText = `
-        width: 200px;
-        height: 400px;
-        background-color: yellow;
-        border: 2px solid black;
-        margin-top: 50px;
-        margin-left: 200px;
-        transition: 200ms;
-    `
-})
-btn2.addEventListener("click", () => {
-    box.style.cssText = `
-        width: 250px;
-        height: 250px;
-        background-color: darkgreen;
-        border: 2px solid black;
-        margin-top: 50px;
-        margin-left: 200px;
-        border-radius: 50%;
-        transition: 200ms;
-    `
-})
+btn.addEventListener("click", () => {
+    if(input.value === " "){
+        alert("Please write something!");
+    }
+    else{
+        container.style.cssText = `
+            border: 1px solid black;
+            margin-top: 50px;
+        `; 
+        const liElement = document.createElement("li");
+        liElement.className = "list-item";
+        liElement.innerText = input.value;
+        ulElement.appendChild(liElement);
+        input.value = " ";
+        liElement.style.cssText = `
+            border: 1px solid red;
+            list-style-type: none;
+            margin: 10px;
+        `;
+    }
+});
