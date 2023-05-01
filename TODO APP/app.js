c4bcdbfff549ab71286cdd6cb70bdf03bfab99bd
Cahangir Kazimzada <78vahjh@code.edu.az>
2023-05-01 12:30:19 +0400

    To do app

--- TODO APP/app.js
+++ TODO APP/app.js
@@ -1,60 +1,57 @@
 const form = document.querySelector(".form-control");
 const input = document.getElementById("input");
-const btn = document.querySelector(".btn");
-const infos = document.querySelector(".infos");
-const ulElement = document.querySelector(".list");
-
-let arr = [];
+const list = document.querySelector(".list");
+const btn = document.querySelector(".addBtn");
+const clear = document.querySelector(".clear-all");
+let memory;
 evenListeners();
 function evenListeners(){
-    form.addEventListener("submit", addInfo)
-};
-
-function addInfo(e){
-    if(input.value === ""){
-        alert("write somtehing");
-    }
-    else{
-        const liElement = document.createElement("li");
-        liElement.className = "list-value";
-        liElement.innerHTML = input.value;
-        infos.style.cssText = `
-        border: 1px solid brown;
-        border-radius: 10px;
-        width: 600px;
-        padding: 20px 100px;
-        `;
-        liElement.style.cssText = `
-        list-style-type: none;
-        border: 1px solid blue;
-        padding: 3px;
-        margin-bottom: 10px;
-        position: relative;
-        `
-        const editBtn = document.createElement("button");
-        const deleteBtn = document.createElement("button");
-        editBtn.innerText = "Edit";
-        deleteBtn.innerText = "Delete";
-        ulElement.appendChild(liElement);
-        ulElement.appendChild(editBtn);
-        ulElement.appendChild(deleteBtn);
-        arr.push(liElement);
-        input.value = "";
-        e.preventDefault();
-        deleteBtn.addEventListener("click", () => {
-            liElement.remove();
-            editBtn.remove();
-            deleteBtn.remove();
-        });
-        editBtn.addEventListener("click", () => {
-            input.value = liElement.innerText;
-            editBtn.addEventListener("click", () => {
-                for(let i = 0; i < arr.length; i++){
-                    arr[i].innerText = input.value;
-                }
-            })
-        })
+    form.addEventListener("submit", addTask);
+    clear.addEventListener("click", clearAll);
+}
+function clearAll(e){
+    alert("Are you sure?");
+    if(confirm){
+        list.remove();
     }
 }
-
-console.log(arr);
+function addTask(e){
+    if(input.value === ""){
+        btn.innerText = "ADD";
+        memory.innerText = input.value;
+        return false;
+    }
+    if(btn.innerText === "ADD"){
+        addTaskToUI(input.value);
+    }
+    else{
+        memory.innerText = input.value;
+        btn.innerText = "ADD";
+        input.value = " ";
+    }
+    e.preventDefault();
+}
+let content = input.value;
+function addTaskToUI(content){
+    const li = document.createElement("li");
+    li.style.listStyleType = "none";
+    const span = document.createElement("span");
+    span.innerText = content;
+    const editBtn = document.createElement("button");
+    editBtn.innerText = "EDIT";
+    editBtn.className = "edit"
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "DELETE";
+    delBtn.className = "delete";
+    delBtn.addEventListener("click", () => {
+        li.remove();
+    });
+    editBtn.addEventListener("click", () => {
+        memory = span;
+        btn.innerText = "EDIT";
+        input.value = span.innerText
+    })
+    li.append(span, editBtn, delBtn);
+    list.appendChild(li);
+    input.value=" ";
+}
